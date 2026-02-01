62c00a70de2be41e296c428df804b61486018697
duyan2509 <22520020@gm.uit.edu.vn>
2026-02-01 14:54:13 +0700

    add serilog project

---
+++ Eventiq.Logging/SerilogExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Eventiq.Logging;
+
+public static class SerilogExtensions
+{
+    public static IHostBuilder UseEventiqSerilog(this IHostBuilder host)
+    {
+        return host.UseSerilog((ctx, services, cfg) =>
+        {
+            cfg
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+
+                .ReadFrom.Configuration(ctx.Configuration)
+                .ReadFrom.Services(services)
+
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Service", ctx.HostingEnvironment.ApplicationName)
+                .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName)
+
+                .WriteTo.Console();
+        });
+    }
+}
