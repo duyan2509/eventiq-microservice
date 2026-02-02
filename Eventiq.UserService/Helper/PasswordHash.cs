c801269f5d933ceff1eb0d7aacc6b0028f31813b
duyan2509 <22520020@gm.uit.edu.vn>
2026-02-02 22:25:11 +0700

    add helper

---
+++ Eventiq.UserService/Helper/PasswordHash.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Eventiq.UserService.Helper;
+
+public static class PasswordHash
+{
+    public static string SHA256Hash(string password)
+    {
+        using var sha = SHA256.Create();
+        var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+        return Convert.ToBase64String(bytes);
+    }
+}
