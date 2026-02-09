c76a5a9f05d2709e6a975d309859491db63d4b9f
duyan2509 <22520020@gm.uit.edu.vn>
2026-02-09 21:42:08 +0700

    fix(user-service): add key attribute and default value

--- Eventiq.UserService/Domain/Entity/BaseEntity.cs
+++ Eventiq.UserService/Domain/Entity/BaseEntity.cs
@@ -5,7 +5,7 @@ namespace Eventiq.UserService.Domain.Entity;
 public abstract class BaseEntity
 {
     [Key]
-    public Guid Id { get; set; } 
+    public Guid Id { get; set; } = Guid.NewGuid();
     
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
