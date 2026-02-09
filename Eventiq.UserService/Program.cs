1a57fb135224d67bed11786201701f7655ce204d
duyan2509 <22520020@gm.uit.edu.vn>
2026-02-09 21:43:08 +0700

    feat(user-service): downgrade masstransit and add enum mapping

--- Eventiq.UserService/Program.cs
+++ Eventiq.UserService/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Eventiq.UserService;
 using Eventiq.UserService.Application.Dto;
 using Eventiq.UserService.Extensions;
@@ -8,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt =>
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.AddApplicationServices();
