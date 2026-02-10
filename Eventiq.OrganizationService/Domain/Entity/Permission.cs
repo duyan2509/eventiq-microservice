e8d74a71b69225afd6dd80e884481f8f4912599b
duyan2509 <22520020@gm.uit.edu.vn>
2026-02-10 22:57:55 +0700

    feat(org-service): add org navigate property

--- Eventiq.OrganizationService/Domain/Entity/Permission.cs
+++ Eventiq.OrganizationService/Domain/Entity/Permission.cs
@@ -3,6 +3,7 @@ public class Permission : BaseEntity
 {
     public string Name { get; set; } = string.Empty;
     public Guid OrganizationId { get; set; }
+    public virtual Organization Organization { get; set; }
     public bool IsDesigner { get; set; } = false;
 }
 
